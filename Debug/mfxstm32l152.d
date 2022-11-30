9d303fd345971a7b7baf955b839d0f73d467d1e3
Piotr Chmielewski <Piotr.Chmielewski@PCOSA.COM.PL>
2022-11-30 14:34:19 +0100

    init

---
+++ Debug/mfxstm32l152.d
@@ -0,0 +1,7 @@
+./debug/mfxstm32l152.o: \
+  C:\Users\p1991\AppData\Local\Arm\Packs\Keil\STM32H7xx_DFP\3.0.0\Drivers\BSP\Components\mfxstm32l152\mfxstm32l152.c \
+  RTE\_Debug\Pre_Include_Global.h \
+  C:\Users\p1991\AppData\Local\Arm\Packs\Keil\STM32H7xx_DFP\3.0.0\Drivers\BSP\Components\mfxstm32l152\mfxstm32l152.h \
+  C:\Users\p1991\AppData\Local\Arm\Packs\Keil\STM32H7xx_DFP\3.0.0\Drivers\BSP\Components\mfxstm32l152\mfxstm32l152_reg.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h
